f8b75507e82ca38726515e36560af08bc192613d
perita23 <hugobedmar10@gmail.com>
2024-10-25 23:38:02 +0200

    quite finish -- have to solve and issue on the adivinarNum method

--- excercici1/scripts/exercici1.js
+++ excercici1/scripts/exercici1.js
@@ -1,20 +1,44 @@
-
+/* Funciones del juego */
 function startGame() {
+    var randomNumber = Math.round(Math.random() * 10);
     var colorDiv = document.getElementById("colorBasedNum");
-    var randomNumber = Math.round((Math.random() * 10));
-    console.log(randomNumber);
+    console.log(randomNumber); // --> Debug en la consola
 
     if (randomNumber >= 5) {
-        colorDiv.style.backgroundColor = "green"
+        colorDiv.style.backgroundColor = "green";
     } else {
-        colorDiv.style.backgroundColor = "red"
+        colorDiv.style.backgroundColor = "red";
     }
 
-    var newWindow = window.open("encerta.html", "_blank", "width=600,height=400");
-    newWindow.onload = function () {
-        var newDoc = newWindow.document;
-        
-    }
+    var encertaHtml = window.open("encerta.html", "_blank", "width=600,height=400");
 
+    encertaHtml.onload = function () {
+        var encertaHtmlDocument = encertaHtml.document;
+        var countText = encertaHtmlDocument.getElementById("cron");
+        var tiempo = 1000;
+
+        var RegresiveCount = setInterval(() => {
+            countText.innerHTML = tiempo;
+            tiempo--;
+
+            if (tiempo < 0) {
+                clearInterval(RegresiveCount);
+                encertaHtml.close();
+            }
+        }, 1000);
+    };
+
+    encertaHtml.adivinarNum = adivinarNum;
+    function adivinarNum() {
+        var encertaHtmlDocument = encertaHtml.document;
+        var clientNum = parseInt(encertaHtmlDocument.getElementById("newNum"))
+        if (clientNum == randomNumber){
+            encertaHtml.close();
+        }else{
+            alert("error");
+        }
+    }
 
 }
+
+
