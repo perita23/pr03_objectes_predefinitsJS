2b9715942a4792d27e33971674e0348d7e3a695f
perita23 <hugobedmar10@gmail.com>
2024-10-28 18:45:50 +0100

    act1 - finished

--- excercici1/scripts/exercici1.js
+++ excercici1/scripts/exercici1.js
@@ -1,7 +1,10 @@
 /* Funciones del juego */
 function startGame() {
+    document.getElementById("history").innerHTML = ""
+
     var randomNumber = Math.round(Math.random() * 10);
     var colorDiv = document.getElementById("colorBasedNum");
+    var intentos = new Array();
     console.log("Número aleatorio generado:", randomNumber); // por si no lo adivinas :)
 
     colorDiv.style.backgroundColor = randomNumber >= 5 ? "green" : "red";
@@ -13,13 +16,15 @@ function startGame() {
         var countText = encertaHtmlDocument.getElementById("cron");
 
         encertaHtml.adivinarNum = adivinarNum; //--> Recordatorio => Esto asigna la funcion a la window del otro html, para que se pueda llamar desde ahi!
-        var tiempo = 30;
+        var tiempo = 7;
         RegresiveCount = setInterval(() => {
             countText.innerHTML = tiempo;
             tiempo--;
             if (tiempo < 0) {
                 clearInterval(RegresiveCount);
                 encertaHtml.close();
+                document.getElementById("history").innerHTML = "<h1>Historial</h1><br><br>" + intentos
+                console.log(document.cookie.split("; "));
             }
         }, 1000);
     };
@@ -29,11 +34,16 @@ function startGame() {
         var clientNum = parseInt(encertaHtmlDocument.getElementById("newNum").value);
 
         if (clientNum === randomNumber) {
-            console.log("correcto")
+            console.log("correcto");
+            intentos.push("<b>Numero insertado =></b> " + clientNum + " -> correcto!<br>");
             encertaHtml.clearInterval(RegresiveCount)
             encertaHtml.close();
+            console.log(document.cookie.split("; "));
+            document.getElementById("history").innerHTML = intentos
         } else {
-            console.log("incorrecto")
+            console.log("incorrecto");
+            intentos.push("<b>Numero insertado =></b> " + clientNum + " -> erroneo!<br>");
         }
+        document.cookie = intentos;
     }
 }
