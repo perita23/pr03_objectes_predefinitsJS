1c92d3b9b67f604f81a3d33bad63a4ca1e1bd3ab
perita23 <hugobedmar10@gmail.com>
2024-11-04 16:00:11 +0100

    exercici1 finished

--- excercici1/scripts/exercici1.js
+++ excercici1/scripts/exercici1.js
@@ -1,3 +1,26 @@
+/* Cookies functions */
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
 /* Funciones del juego */
 function startGame() {
     document.getElementById("history").innerHTML = ""
@@ -16,7 +39,7 @@ function startGame() {
         var countText = encertaHtmlDocument.getElementById("cron");
 
         encertaHtml.adivinarNum = adivinarNum; //--> Recordatorio => Esto asigna la funcion a la window del otro html, para que se pueda llamar desde ahi!
-        var tiempo = 7;
+        var tiempo = 10;
         RegresiveCount = setInterval(() => {
             countText.innerHTML = tiempo;
             tiempo--;
@@ -25,6 +48,7 @@ function startGame() {
                 encertaHtml.close();
                 document.getElementById("history").innerHTML = "<h1>Historial</h1><br><br>" + intentos
                 console.log(document.cookie.split("; "));
+                document.cookie = intentos.toString();
             }
         }, 1000);
     };
@@ -34,16 +58,12 @@ function startGame() {
         var clientNum = parseInt(encertaHtmlDocument.getElementById("newNum").value);
 
         if (clientNum === randomNumber) {
-            console.log("correcto");
             intentos.push("<b>Numero insertado =></b> " + clientNum + " -> correcto!<br>");
             encertaHtml.clearInterval(RegresiveCount)
             encertaHtml.close();
-            console.log(document.cookie.split("; "));
             document.getElementById("history").innerHTML = intentos
         } else {
-            console.log("incorrecto");
             intentos.push("<b>Numero insertado =></b> " + clientNum + " -> erroneo!<br>");
         }
-        document.cookie = intentos;
     }
 }
