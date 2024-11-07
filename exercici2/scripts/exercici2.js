979e2bedfd1c2fa38af9e7f2018f6b947b0a96ca
perita23 <hugobedmar10@gmail.com>
2024-11-07 19:19:53 +0100

    game finished without cookies

--- exercici2/scripts/exercici2.js
+++ exercici2/scripts/exercici2.js
@@ -1,40 +1,63 @@
-var RegresiveCount;
+/*Global Var 
+ * @param RegresiveCount => Cuenta atras
+ * @param colorList => array de colores
+ * @param windowList => array de ventanas abiertas
+ * @param actualWindow => ventana clicada en la primera vez, esta se usa para verificar si se habia clicado una ventana antes o no
+ * @param openedWindows => contador del total de ventanas que se han abierto durante la partida
+ * @param ventanasUnclosed => contador de ventanas sin cerrar durante el juego, se usa para saber si has ganado o perdido
+ */
+var regresiveCount;
 var colorList = ["red", "blue", "green", "yellow"];
 var windowList = [];
 var actualWindow = null;
 var openedWindows = 0;
+var ventanasUnclosed = 0;
 
+/** Generador de numeros random
+ * @param min => valor minimo que puede devolver la funcion
+ * @param max => valor maximo que puede devovler la funcion
+ */
 function randomNumber(min, max) {
     return Math.floor(Math.random() * max) + min;
 }
 
+/**
+ * Recibe una ventana, verifica si ya hay una ventana guardada y decide si comparar o esperar a la siguiente ventana
+ * 
+ * @param ventana => ventana que recibe la funcion, al llamar a esta funcion se pasa la ventana desde la cual se ha llamado a la funcion
+ */
 function checkColors(ventana) {
-    console.log("funciona")
     if (actualWindow == null) {
         actualWindow = ventana;
     } else {
         color1 = actualWindow.document.getElementById("colorText").getHTML();
         color2 = ventana.document.getElementById("colorText").getHTML();
         if (ventana == actualWindow) {
+            let randColor = randomNumber(0, 4);
+            ventana.document.getElementById("colorText").innerHTML = colorList[randColor];
+            ventana.document.body.style.backgroundColor = colorList[randColor];
             windowGenerator(1);
         }
         if (color1 == color2 && ventana != actualWindow) {
-            ventana.close();
-            actualWindow.close();
-            actualWindow = null;
-
             let index1 = windowList.indexOf(actualWindow);
             windowList.splice(index1, 1);
             let index2 = windowList.indexOf(ventana);
             windowList.splice(index2, 1);
 
-            console.log(windowList)
+            ventana.close();
+            actualWindow.close();
+            actualWindow = null;
+
+            console.log("validando activeWindows")
+            validateActiveWindows()
         } else {
             actualWindow = null
         }
     }
 }
-
+/** Generador de ventanas
+ * @param rand => numero de ventanas que se han de generar
+ */
 function windowGenerator(rand) {
     for (let index = 0; index < rand; index++) {
         let randColor = randomNumber(0, 4);
@@ -55,33 +78,85 @@ function windowGenerator(rand) {
     }
     console.log(windowList)
 }
-
+/**
+ * Empieza el juego, empieza la cuenta atras y el interval que se ejecuta cada segundo.
+ * Durante los tres primeros segundos se van a generar nuemeros aleatorios de ventanas.
+ */
 function startGame() {
-    var time = 30;
-    var count = document.getElementById("count")
+    var TIME = 220;
+    var spawnTime = TIME - 3;
+    var won = null;
+    ventanasUnclosed = 0;
 
-    if (RegresiveCount == null) {
-        console.log("starting game")
-        count.innerHTML = time;
-        RegresiveCount = setInterval(() => {
-            if (time == 0) {
-                clearInterval(RegresiveCount);
-                windowList.forEach(element => {
-                    element.close()
-                });
-                console.log(openedWindows);
-                alert("Se acabó!")
-            }
-            if (time >= 27) {
+    var count = document.getElementById("count");
+    var btnText = document.getElementById("btnText");
+    var infoGame = document.getElementById("infoGame");
+
+    // btnText.innerHTML = "Empezar Juego";
+    infoGame.innerHTML = "";
+
+    if (regresiveCount == null) {
+        btnText.innerHTML = "Jugando...";
+        count.innerHTML = TIME;
+
+        regresiveCount = setInterval(() => {
+            if (TIME >= spawnTime) {
                 let rand = randomNumber(1, 5);
-                console.log(rand)
                 windowGenerator(rand);
             }
-            time--;
-            count.innerHTML = time;
+
+            if (TIME == 0) {
+                finishGame()
+            }
+            TIME--;
+            count.innerHTML = TIME;
         }, 1000);
     } else {
         console.log("There's an active interval")
     }
 
+}
+/**
+ * Valida cuantas ventanas activas hay, si son 0 (es decir no hay ventanas activas) finaliza el juego
+ */
+function validateActiveWindows() {
+    ventanasUnclosed = 0;
+    windowList.forEach(element => {
+        if (element != null) {
+            ventanasUnclosed++
+        }
+    })
+    console.log(ventanasUnclosed)
+    if (ventanasUnclosed == 0) {
+        finishGame()
+    }
+}
+/**
+ * Acaba el juego, limpia el intervalo y cierra todas las ventanas que han quedado abiertas (No quiero explotarle el pc al profe :c )
+ */
+function finishGame() {
+    clearInterval(regresiveCount);
+    regresiveCount = null;
+    closeAllWindows();
+
+    // console.log(openedWindows);
+
+    btnText.innerHTML = "Reintentar";
+    if (ventanasUnclosed == 0) {
+        infoGame.innerHTML = "Has ganado!";
+    } else {
+        infoGame.innerHTML = "Se acabó el tiempo <br>Has perdido!"
+    }
+
+}
+/**
+ * cierra todas las ventanas activas
+ */
+function closeAllWindows() {
+    windowList.forEach(element => {
+        if (element != null) {
+            ventanasUnclosed++;
+            element.close()
+        }
+    });
 }
